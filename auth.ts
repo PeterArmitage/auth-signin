276bca159fcb864caaa7171d80d5f584915b453d
Peter Armitage <ficochu84@gmail.com>
2024-04-25 16:58:02 -0300

    add github and google auth

--- auth.ts
+++ auth.ts
@@ -1,14 +1,35 @@
 import NextAuth from 'next-auth';
 import authConfig from './auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { getUserById } from './data/user';
 import prisma from './lib/db';
+import { UserRole } from '@prisma/client';
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+	events: {
+		async linkAccount({ user }) {
+			await prisma.user.update({
+				where: { id: user.id },
+				data: { emailVerified: new Date() },
+			});
+		},
+	},
 	callbacks: {
 		async session({ token, session }) {
+			if (token.sub && session.user) {
+				session.user.id = token.sub;
+			}
+			if (token.role && session.user) {
+				session.user.role = token.role as UserRole;
+			}
 			return session;
 		},
 		async jwt({ token }) {
+			if (!token.sub) return token;
+
+			const existingUser = await getUserById(token.sub);
+			if (!existingUser) return token;
+			token.role = existingUser.role;
 			return token;
 		},
 	},
